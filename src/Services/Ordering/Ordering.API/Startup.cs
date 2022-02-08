9f4410bb714329792281780dde60ff92486f1d95
Bruno Liapis <liapisb@gmail.com>
2022-02-08 12:58:18 -0300

    added presentation layer controller added order repository added email service implementation with sendgrid

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.Application;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,12 +26,14 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
             });
+
+            ApplicationServicesRegistration.AddApplicationSerevices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
